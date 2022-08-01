fb04aeca5291a08e1452d124a544a6f78acde770
Dominic Schmid <dominic.schmid@hotmail.com>
2022-08-01 22:45:09 +0200

    Remove API keys from Git

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyAitjSGptIa2zSrUjfqfkXwgdW-MEbxPjU")
+    GMSServices.provideAPIKey("AIzaSyD9PF9UECuGsHiCYRebO6OH6alK3b0ZoSc")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
