1a22bc2d2908fdcc77c37e3644335938e02d5bef
Dominic Schmid <dominic.schmid@hotmail.com>
2022-08-01 22:47:50 +0200

    Revert "Remove API keys from Git"
    This reverts commit fb04aeca5291a08e1452d124a544a6f78acde770.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyD9PF9UECuGsHiCYRebO6OH6alK3b0ZoSc")
+    GMSServices.provideAPIKey("AIzaSyAitjSGptIa2zSrUjfqfkXwgdW-MEbxPjU")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
